e51b2ba50d45fe739ccf1663402e502f44878280
Michaelninder <fabianeternis@gmail.com>
2025-08-05 15:55:48 +0200

    locale...

--- app/Models/SiteView.php
+++ app/Models/SiteView.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class SiteView extends Model
 {
@@ -12,16 +14,34 @@ class SiteView extends Model
     protected $fillable = [
         'site_id',
         'device_id',
-        'ip_address',
+        'route',
         'viewed_at',
     ];
 
-    public function site()
+    protected $casts = [
+        'viewed_at' => 'datetime',
+    ];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function site(): BelongsTo
     {
         return $this->belongsTo(Site::class);
     }
 
-    public function device()
+    public function device(): BelongsTo
     {
         return $this->belongsTo(Device::class);
     }
